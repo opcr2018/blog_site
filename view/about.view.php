3c440c4786b176085da4a7dbb909e48f4b9b8587
opcr2018 <haksaeng.2018@gmail.com>
2020-01-29 13:16:29 +0100

    mise à jour de about

--- view/about.view.php
+++ view/about.view.php
@@ -3,67 +3,189 @@
 
 
 <div class="jumbotron text-center" id="about">
-    <h1>Curriculum Vitae</h1>
+  <h1>Curriculum Vitae</h1>
 </div>
 
 <div class="accordion" id="resume">
   <div class="card">
     <div class="card-header" id="headingOne">
       <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#formation" aria-expanded="true" aria-controls="formation">
-          Formation
+        Formation <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#formation"
+          aria-expanded="true" aria-controls="formation">
+          🔻
         </button>
       </h2>
-    </div>
-
-    <div id="formation" class="collapse show" aria-labelledby="headingOne" data-parent="#resume">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        Les études et formations que j'ai suivies sont un mélange de technique industrielle (informatique,
+        programmation, télécommunication cryptée, prototype) et d'artisanat (stylisme, études des couleurs, création de
+        vetements sur mesure).
+      </div>
+    </div>
+    <div id="formation" class="collapse" aria-labelledby="headingOne" data-parent="#resume">
+      <div class="card-body">
+        <ul class="liststyle1">
+          <li><strong> OpenclassRooms : </strong>Formation de Developpeur d'Application WEB - PHP / Symfony</li>
+          <li><strong> CNFDI : </strong>Certificat de sercrétaire assistante comptable</li>
+          <li><strong> AFPA : </strong>Brevet de technicien en modélisme patronnage et gradation artisanale et
+            industrielle (Gerber)</li>
+          <li><strong> Armée de Terre : </strong>formation militaire en communication cryptée et bureautique</li>
+          <li><strong> Lycée Honoré Baradat : </strong>CAP/BEP en stylisme, modélisme et gradation artisanale et
+            industrielle</li>
+        </ul>
       </div>
     </div>
   </div>
   <div class="card">
     <div class="card-header" id="headingTwo">
       <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#competence" aria-expanded="false" aria-controls="competence">
-          Compétence
+        Compétences<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#competence"
+          aria-expanded="false" aria-controls="competence">🔻
         </button>
       </h2>
+      <div class="card-body">
+        Mes compétences touchent trois domaines différents : le domaine du software, le domaine du hardware et le
+        domaine de la bureautique.
+      </div>
     </div>
     <div id="competence" class="collapse" aria-labelledby="headingTwo" data-parent="#resume">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="row">
+          <div class="col-md-6">
+            <ul class="liststyle2">
+              <li>Software :</li><br />
+              <ul class="liststyle1-2">
+                <li>
+                  Déployer un site sur un hébergeur distant
+                </li><br />
+                <li>
+                  Maintenir un post de travail (mise à jour, nettoyage)
+                </li><br />
+                <li>
+                  Installer des périphériques (imprimantes, matériel multimédias...)
+                </li><br />
+                <li>
+                  Sauvegarder l'image disque avec Redo & Backup
+                </li><br />
+              </ul>
+              <li>Hardware : </li>
+              <ul class="liststyle2-2">
+                <li>
+                  Installer poste informatique
+                </li><br />
+                <li>
+                  Configurer réseau domestique
+                </li><br />
+                <li>
+                  Assembler
+                </li><br />
+                <li>
+                  Diagnostiquer les pannes
+                </li><br />
+              </ul>
+              <li>Bureautique :</li><br />
+              <ul class="liststyle2-4">
+                <li>Gestion électronique des documents</li><br />
+                <li>Etablir un cahier des charges avec maquette fil de fer</li><br />
+                <li>Créer et utiliser des macros pour traiter des données sous Excel</li><br />
+                <li>Créer des documents et modèles de documents</li><br />
+                <li>Créer un modèle de document pour un publipostage</li><br />
+              </ul>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <ul class="liststyle2">
+              <li>Créer et mettre à jour un site internet (Html5, Css3, Bootstrap)</li><br />
+              <li>Configurer un CMS (Wordpress, Prestashop)</li><br />
+              <li>Rechercher des solutions en ligne et veille technologique</li><br />
+              <li>Logiciels utilisés régulièrement :</li><br />
+              <ul class="liststyle2-3">
+                <li>Visual Code</li><br />
+                <li>Navigateur Opera</li><br />
+                <li>Wamp</li><br />
+                <li>FileZilla Client</li><br />
+                <li>WorkBench</li><br />
+                <li>Suite Office & Outlook</li><br />
+                <li>Gimp, IrfanView</li><br />
+                <li>Skype, Discord, Hangouts</li><br />
+                <li>SpyBot, Ccleaner</li><br />
+              </ul>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#experience" aria-expanded="false" aria-controls="experience">
+    <div class="card">
+      <div class="card-header" id="headingThree">
+        <h2 class="mb-0">
           Expériences professionnelles
-        </button>
-      </h2>
-    </div>
-    <div id="experience" class="collapse" aria-labelledby="headingThree" data-parent="#resume">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#experience"
+            aria-expanded="false" aria-controls="experience">🔻
+          </button>
+        </h2>
+        <div class="card-body">
+          Mon expérience professionnelle est riche parce que je suis curieuse autant du contenu du poste que de
+          l'environnement.
+        </div>
+      </div>
+      <div id="experience" class="collapse" aria-labelledby="headingThree" data-parent="#resume">
+        <div class="card-body">
+          <ul class="liststyle3">
+            <li></li><br />
+            <li></li><br />
+            <li></li><br />
+            <li></li><br />
+            <li></li><br />
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingFour">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#divers" aria-expanded="false" aria-controls="divers">
+    <div class="card">
+      <div class="card-header" id="headingFour">
+        <h2 class="mb-0">
           Divers
-        </button>
-      </h2>
-    </div>
-    <div id="divers" class="collapse" aria-labelledby="headingFour" data-parent="#resume">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#divers"
+            aria-expanded="false" aria-controls="divers">🔻
+          </button>
+        </h2>
+        <div class="card-body">
+          Ici sont évoqués les langues étrangères, le permis de conduire et mes passe-temps.
+        </div>
+      </div>
+      <div id="divers" class="collapse" aria-labelledby="headingFour" data-parent="#resume">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-6">
+              <ul class="liststyle4">
+                <li>Langues étrangères :</li>
+                <ul>
+                  <p>
+                    - anglais : maitrise<br />
+                    - allemand : intermédiaire<br />
+                    - coréen : débutant
+                  </p>
+                </ul>
+                <li>
+                  Permis de conduire B
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <ul>
+                <li class="liststyle4">Hobbies</li>
+                <ul>
+                  <p>
+                    - jeux en ligne (FFXIV - EternalMagic)<br />
+                    - lecture lightnovel asiatique traduit en anglais<br />
+                  </p>
+                </ul>
+              </ul>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
-</div>
-<?php $contentPage = ob_get_clean();
+  <?php $contentPage = ob_get_clean();
 include(VIEW . 'template.php');
